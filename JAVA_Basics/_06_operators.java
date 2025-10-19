76979140c703e8f647c07deb90818f18aa11c2a7
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-19 14:47:33 +0000

    file

--- JAVA_Basics/_06_operators.java
+++ JAVA_Basics/_06_operators.java
@@ -1,6 +1,6 @@
 //Assignment operator:
 /*
- 
+ Unary Operators: works on single operator, increment,decrement,etc
  */
 public class _06_operators {
     public static void main(String args[]){
@@ -8,5 +8,20 @@ public class _06_operators {
         System.out.println(myInt);
         int newint=myInt;
         System.out.println(newint);
+
+        int x=5;
+        int y=-x;
+        int z=-y;
+        System.out.println(y);
+
+        int a=5;
+        a=a+1;
+        System.out.println(a);
+        a+=1;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+        ++a;
+        System.out.println(++a);
     }
 }
---
+++ Questions/_03_shorthand_operator.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class _03_shorthand_operator {
+    public static void main(String args[]){
+        int a=6;
+        Scanner input=new Scanner(System.in);
+        int x1=input.nextInt();
+        a=a+x1;
+        System.out.println(a);
+        int x2=input.nextInt();
+        a=a+x2;
+        System.out.println(a);
+
+    }
+}
