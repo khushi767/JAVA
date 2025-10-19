ebd5ce9d92f014242c7e32da46812093c9c53f11
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-19 14:36:08 +0000

    operators

---
+++ JAVA_Basics/_06_operators.java
@@ -0,0 +1,12 @@
+//Assignment operator:
+/*
+ 
+ */
+public class _06_operators {
+    public static void main(String args[]){
+        int myInt=9;
+        System.out.println(myInt);
+        int newint=myInt;
+        System.out.println(newint);
+    }
+}
---
+++ Questions/_02_swap_two_numbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class _02_swap_two_numbers {
+    public static void main(String args[]){
+       Scanner input=new Scanner(System.in);
+       System.out.print("Enter value of a: ");
+       int a=input.nextInt();
+       System.out.print("Enter value of b: ");
+       int b=input.nextInt();
+        int c=a;
+        a=b;
+        b=c;
+        System.out.println("Swapping done... ");
+        System.out.println("Value of A is: "+a);
+        System.out.println("Value of b is: "+b);
+    }
+}
