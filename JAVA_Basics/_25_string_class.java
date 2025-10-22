781e11963e80d263b9dd786106bd613189fe5c9b
khushi kumari <99918465+khushi767@users.noreply.github.com>
2025-10-22 05:42:27 +0000

    .

--- JAVA_Basics/_25_string_class.java
+++ JAVA_Basics/_25_string_class.java
@@ -4,7 +4,27 @@
  * %e:Exponential,scientific notation of float or double
  * %b: boolean true or false value
  * %c: single character char
- * %d: 
+ * %d: base 10 integer value(byte,short,int,long)
+ * %o: octal number
+ * %x: hexadecimal numbr
+ * %%: percentage sign
+ * %n: new line
+ * %tY: year to four digits
+ * %tR: time in 24-hour notation hh:mm
+ * %tT: time in hh:mm:ss
+ * %tD: date in mm/dd/yy
+ * %tF: date in yyyy-mm-dd
+ * %tA: full weekday name
+ * 
+ * 
+ * Printf fkag: Purpose
+ * - : Aligns the formatted printf output to left
+ * + : output includes a negative or positive sign
+ * ( :places negative number in parenthesis
+ * 0:formatted printf output is zero padded
+ * ,: formatted output includes grouping seperators
+ * <space>: a blank space adds a minus sign for negatibe numbers and a leading space when positive.
+ * 
  */
 
 public class _25_string_class {
@@ -12,6 +32,6 @@ public class _25_string_class {
     String name="khushi";
     int marks=90;
     System.out.println("hello"+name+",your marks are: "+45);
-    System.out.printf("hello %s, your marks are: %d",name,marks);
+    System.out.printf("hello %s, your marks are: %d",name,marks);// this is more effiicient as we dont have to concatenate strings
    } 
 }
